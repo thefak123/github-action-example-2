2629c67dc39039d29bb7facc8c65d0b11e71aa7c
greatdesigners12 <jackquintine3456@gmail.com>
2024-07-18 15:33:37 +0700

    make it work again

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT;
 //
 // HTTP GET route we can use to check if the service is handling requests.
 //
-app.get("/livebro", (req, res) => {
+app.get("/live", (req, res) => {
     res.sendStatus(200);
 });
 
